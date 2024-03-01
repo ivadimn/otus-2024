a642f5f68827db80b7d978f753b0e6dd82a5a487
vadim <ivadimn@mail.ru>
2024-03-01 21:36:13 +0300

    homework 09

--- hw09/main_old.asm
+++ hw09/main_old.asm
@@ -1,23 +1,19 @@
-bits 64
-extern malloc, puts, printf, fflush, abort
-global main
+    bits 64
+    extern malloc, puts, printf, fflush, abort
+    global main
 
-
-section   .data
+    section   .data
 empty_str: db 0x0
 int_format: db "%ld ", 0x0
 data: dq 4, 8, 15, 16, 23, 42
 data_length: equ ($-data) / 8
 
-
-section .text
-
+    section   .text
 ;;; print_int proc
 print_int:
     push rbp
-    push rax
-    push rdi
-    push rsi
+    mov rbp, rsp
+    sub rsp, 16
 
     mov rsi, rdi
     mov rdi, int_format
@@ -27,9 +23,7 @@ print_int:
     xor rdi, rdi
     call fflush
 
-    pop rsi
-    pop rdi
-    pop rax
+    mov rsp, rbp
     pop rbp
     ret
 
@@ -38,13 +32,12 @@ p:
     mov rax, rdi
     and rax, 1
     ret
-    
+
 ;;; add_element proc
 add_element:
     push rbp
     push rbx
     push r14
-        
     mov rbp, rsp
     sub rsp, 16
 
@@ -63,30 +56,35 @@ add_element:
     pop r14
     pop rbx
     pop rbp
-    ret    
-    
+    ret
+
 ;;; m proc
 m:
     push rbp
-    push rax
-    push rbx
-    push r12
-    
-print_loop:
+    mov rbp, rsp
+    sub rsp, 16
+
     test rdi, rdi
     jz outm
+
+    push rbp
+    push rbx
+
     mov rbx, rdi
-    
+    mov rbp, rsi
+
     mov rdi, [rdi]
-    call print_int
+    call rsi
 
     mov rdi, [rbx + 8]
-    jmp print_loop
+    mov rsi, rbp
+    call m
+
+    pop rbx
+    pop rbp
 
 outm:
-    pop r12
-    pop rbx
-    pop rax        
+    mov rsp, rbp
     pop rbp
     ret
 
@@ -94,44 +92,45 @@ outm:
 f:
     mov rax, rsi
 
+    test rdi, rdi
+    jz outf
+
     push rbx
     push r12
     push r13
-    
-add_loop:
-    
-    test rdi, rdi
-    jz outf
-        
+
     mov rbx, rdi
     mov r12, rsi
-    
+    mov r13, rdx
+
     mov rdi, [rdi]
-    call p
+    call rdx
     test rax, rax
     jz z
-    
+
     mov rdi, [rbx]
     mov rsi, r12
     call add_element
     mov rsi, rax
     jmp ff
-    
+
 z:
     mov rsi, r12
-    
+
 ff:
     mov rdi, [rbx + 8]
-    jmp add_loop;
-    
-outf:
+    mov rdx, r13
+    call f
+
     pop r13
     pop r12
     pop rbx
+
+outf:
     ret
 
+;;; main proc
 main:
-    mov rbp, rsp; for correct debugging
     push rbx
 
     xor rax, rax
@@ -142,20 +141,23 @@ adding_loop:
     call add_element
     dec rbx
     jnz adding_loop
-        
+
     mov rbx, rax
+
     mov rdi, rax
+    mov rsi, print_int
     call m
 
     mov rdi, empty_str
     call puts
 
+    mov rdx, p
     xor rsi, rsi
     mov rdi, rbx
     call f
 
-    mov rdi, rsi
-    mov rbx, rsi
+    mov rdi, rax
+    mov rsi, print_int
     call m
 
     mov rdi, empty_str
